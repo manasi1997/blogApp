722fdbb4ee57497193721cd713b886c145beb955
Manasi Dattatraya Pawar <pawarmanasi567@gmail.com>
2024-06-14 20:25:37 +0530

    Create signup api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,20 @@
-
-import { mongoose } from "mongoose";
-
+import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        require: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        require: true,
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
