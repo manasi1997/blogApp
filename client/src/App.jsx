6537ba2b9e7a1ac9ffb5de3f0fe475f8b1ee3d18
Manasi Dattatraya Pawar <pawarmanasi567@gmail.com>
2024-06-25 12:23:41 +0530

    add the functionality to the sign-up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import Products from './pages/Products'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path='/products' element={<Products />}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
